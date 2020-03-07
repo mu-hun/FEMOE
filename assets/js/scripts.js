518afc0cb10d11941580fd7a940439f75bf552da
Muhun Kim <iam@muhun.kim>
2020-03-07 10:09:20 +0900

    Hugo can't access template variable in js file
    HACK: FIX TODO

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,25 +2,3 @@ if (!(/*@cc_on!@*/false || !!document.documentMode) && !!window.StyleMedia) {
     document.body.classList.toggle('.win');
 }
 
-(function () {
-	// Do not run below code at localhost
-
-    if (window.location.hostname == "localhost")
-         return;
-
-	// Google Analytics
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-	gtag('config', '{{ .Site.googleAnalytics }}');
-	
-	// Disqus
-	var disqus_loaded = false;
-
-    var e = document.createElement("script");
-    e.type = "text/javascript";
-    e.async = true;
-    e.src = "//" + '{{ .Site.DisqusShortname }}' + ".disqus.com/embed.js";
-    document.getElementsByTagName("body")[0]
-    .appendChild(e);
-})();
